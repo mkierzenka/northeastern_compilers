99ae56254ff928e7c3002203cb9e72d3b570a958
Ian Dardik <iandardik@login-students.ccs.neu.edu>
2022-01-29 07:42:49 -0500

    added some more tests

--- starter-adder/test.ml
+++ starter-adder/test.ml
@@ -19,9 +19,17 @@ let tie (filename : string) (expected_err : string) = filename>::test_err_input 
 let suite : OUnit2.test =
 "suite">:::
  [t "forty_one" "41" "41";
+  t "let_const" "(let ((a 7)) 71)" "71";
+  t "add1" "(add1 4)" "5";
+  t "sub1" "(sub1 4)" "3";
+  t "sub_add" "(add1 (sub1 44))" "44";
+  t "add_sub" "(sub1 (add1 40))" "40";
+  t "add_add" "(add1 (add1 111))" "113";
+  t "sub_sub" "(sub1 (sub1 1000))" "998";
 
   t "nyi" "(let ((x 10)) x)" "10";
 
+  te "let_empty" "(let (()) 2)" "Syntax error on bindings";
   ]
 ;;
 
