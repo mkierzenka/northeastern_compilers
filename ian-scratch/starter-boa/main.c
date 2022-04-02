baf89835d74e67c526d3c3bc2074558844f2bd9d
Marcin Kierzenka <33709159+mkierzenka@users.noreply.github.com>
2022-04-02 10:15:43 -0400

    removed ian-scratch directory

--- ian-scratch/starter-boa/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-extern int64_t our_code_starts_here() asm("our_code_starts_here");
-
-int main(int argc, char** argv) {
-  printf("%ld\n", our_code_starts_here());
-  return 0;
-}
