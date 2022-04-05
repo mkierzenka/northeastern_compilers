12086fd2f582074772b38d331f52f2a4ef1e25a6
Marcin Kierzenka <33709159+mkierzenka@users.noreply.github.com>
2022-04-05 18:55:43 -0400

    Updated some tests to match our new well-formed rules/messages, still getting failures though

--- fer-de-lance/test.ml
+++ fer-de-lance/test.ml
@@ -275,6 +275,7 @@ let tests_from_cobra = [
 
 let cobra_suite = "cobra_suite">:::tests_from_cobra
 
+let multi_errs_code_str = "def f(x, x): y  f(1)"
 
 (* ### Regression test suite (from Diamondback) ### *)
 let tests_from_diamondback = [
@@ -309,21 +310,21 @@ let tests_from_diamondback = [
   t "func_from_func" "def f(x,y): let z = x * y in z + z  and def g(x): if isbool(x): 0 else: f(x,x) g(4)"  "" "32";
   terr "func_from_func_dif_grp" "def f(x,y): let z = x * y in z + z  def g(x): if isbool(x): 0 else: f(x,x) g(4)"  "" "Name f not found";
   t "func_from_func_backwards" "def f(): g()  and def g(): sub1(2 * 8)  f()"  "" "15";
-  terr "func_from_func_backwards_dif_grp" "def f(): g()  def g(): sub1(2 * 8)  f()"  "" "Name g not found";
+  terr "func_from_func_backwards_dif_grp" "def f(): g()  def g(): sub1(2 * 8)  f()"  "" "The identifier g";
   t "max_tail_1" "def max(x,y): if x > y: x else: max(y,x) max(1,9)"  "" "9";
   t "max_tail_2" "def max(x,y): if x > y: x else: max(y,x) max(9,1)"  "" "9";
   t "rebind_arg" "def f(a,b): let a=b,b=8 in a+b f(4,10)"  "" "18";
 
   (* tail call testing *)
-  t "tail_larger_arity_2_to_3" "def f(x,y): g(x,y,4)  def g(a,b,c): a - b + c  f(2,5)"  "" "1";
-  t "tail_larger_arity_3_to_4" "def f(x,y,z): g(x,4,z,y)  def g(a,b,c,d): a - b + c - d  f(1,2,3)"  "" "-2";
-  t "tail_larger_arity_2_to_4" "def f(x,y): g(x,-9, y,4)  def g(a,b,c,d): a - b + c - d  f(2,5)"  "" "12";
-  t "tail_same_arity_1" "def f(x): g(x + 8)  def g(x): x - 2  f(4)"  "" "10";
-  t "tail_same_arity_2" "def f(x,y): g(sub1(x),add1(y))  def g(x,y): x * y  f(3,5)"  "" "12";
-  t "tail_same_arity_3" "def f(x,y,z): g(x,z,y)  def g(a,b,c): (a + b) * c  f(2,2,4)" ""  "12";
-  t "tail_smaller_arity_3_to_2" "def f(i,j,k): g(i+j,k)  def g(i,k): i * k  f(0,2,2)" ""  "4";
-  t "tail_smaller_arity_4_to_3" "def f(i,j,k,hi): g(i+j,k*hi,hi)  def g(i,k,x): x + (i*k)  f(0,2,2,1)"  "" "5";
-  t "tail_smaller_arity_4_to_2" "def f(i,j,k,hi): g(i+j,k*hi)  def g(i,k): 3 + (i*k)  f(0,2,2,1)"  "" "7";
+  t "tail_larger_arity_2_to_3" "def f(x,y): g(x,y,4) and def g(a,b,c): a - b + c  f(2,5)"  "" "1";
+  t "tail_larger_arity_3_to_4" "def f(x,y,z): g(x,4,z,y) and def g(a,b,c,d): a - b + c - d  f(1,2,3)"  "" "-2";
+  t "tail_larger_arity_2_to_4" "def f(x,y): g(x,-9, y,4) and def g(a,b,c,d): a - b + c - d  f(2,5)"  "" "12";
+  t "tail_same_arity_1" "def f(x): g(x + 8) and def g(x): x - 2  f(4)"  "" "10";
+  t "tail_same_arity_2" "def f(x,y): g(sub1(x),add1(y)) and def g(x,y): x * y  f(3,5)"  "" "12";
+  t "tail_same_arity_3" "def g(a,b,c): (a + b) * c  def f(x,y,z): g(x,z,y)  f(2,2,4)" ""  "12";
+  t "tail_smaller_arity_3_to_2" "def g(i,k): i * k  and def f(i,j,k): g(i+j,k)  f(0,2,2)" ""  "4";
+  t "tail_smaller_arity_4_to_3" "def f(i,j,k,hi): g(i+j,k*hi,hi) and def g(i,k,x): x + (i*k)  f(0,2,2,1)"  "" "5";
+  t "tail_smaller_arity_4_to_2" "def f(i,j,k,hi): g(i+j,k*hi) and def g(i,k): 3 + (i*k)  f(0,2,2,1)"  "" "7";
 
   (* Asserting that functions don't leak envs *)
   terr "func_split_env_err" "def f(x,y): let z = x * y in sub1(z)   let z = 9 in f(x, add1(z))"  "" "is not in scope";
@@ -344,17 +345,17 @@ let tests_from_diamondback = [
   terr "arity_err2b" "def f(x,y): (if isnum(12): 7 else: false)  f(1,3,4)"  "" "arity mismatch in call";
 
   (* Name resolution for vars vs. funcs (vars shadow function names) *)
-  terr "func_let_name" "def f(x,y): (if isnum(12): 7 else: false)  let f=99 in f(1,3)"  "" "is not in scope";
-  terr "func_let_name2" "def f(x,y): (if isnum(12): 7 else: false)  let f=99 in f(1,3,4)"  "" "is not in scope";
+  terr "func_let_name" "def f(x,y): (if isnum(12): 7 else: false)  let f=99 in f(1,3)"  "" "tried to call non-closure value";
+  terr "func_let_name2" "def f(x,y): (if isnum(12): 7 else: false)  let f=99 in f(1,3,4)"  "" "tried to call non-closure value";
   t "func_let_name3" "def f(x,y): (if isnum(12): 7 else: false)  let f=99 in add1(f)" ""  "100";
 
 
   (* UnboundFun errors *)
-  terr "unbound_fun_l" "f()"  "" "The function name f, used at";
+  terr "unbound_fun_l" "f()"  "" "The identifier f, used at";
   terr "unbound_fun_r" "f()"  "" ", is not in scope";
-  terr "unbound_fun" "def f(): add1(4 * 2)  def g(): sub1(2 * 8)  if false: h() else: 9" ""  "The function name h";
-  terr "unbound_fun_in_decl" "def f(): add1(4 * 2)  def g(): h()  7"  "" "The function name h";
-  terr "unbound_fun_let" "def f(): add1(4 * 2)  def g(): sub1(2 * 8) let h=true in h()"  "" "The function name h";
+  terr "unbound_fun" "def f(): add1(4 * 2)  def g(): sub1(2 * 8)  if false: h() else: 9" ""  "is not in scope";
+  terr "unbound_fun_in_decl" "def f(): add1(4 * 2)  def g(): h()  7"  "" "not in scope";
+  terr "unbound_fun_let" "def f(): add1(4 * 2)  def g(): sub1(2 * 8) let h=true in h()"  "" "Tried to call non-closure value";
 
   (* UnboundId errors *)
   terr "unbound_id_l" "a"  "" "The identifier a, used at";
@@ -374,6 +375,7 @@ let tests_from_diamondback = [
   terr "duplicate_fun_r" "def f(): 2 def f(): 3 8"  "" ", duplicates one at";
   terr "duplicate_fun_2_l" "def f(a,b): 2 def f(): 3 8"  "" "The function name f, redefined at";
   terr "duplicate_fun_2_r" "def f(a,b): 2 def f(): 3 8"  "" ", duplicates one at";
+  terr "duplicate_fun_3" "def f(): 2 and def g(): 1 and def f(): 3   8"  "" "duplicate";
 
   (* Overflow errors were tested in Cobra *)
 
@@ -383,13 +385,10 @@ let tests_from_diamondback = [
   terr "unbound_duplicate_id_dupl" "let x=1,x=2 in y"  "" "The identifier x, redefined at";
   terr "unbound_duplicate_id_dupr" "let x=1,x=2 in y"  "" ", duplicates one at";
 
-  terr "multi_errs_unbound_l" "def f(x, x): y  f(1)"  "" "The identifier y, used at ";
-  terr "multi_errs_unbound_r" "def f(x, x): y  f(1)"  "" ", is not in scope";
-  terr "multi_errs_duplicated_l" "def f(x, x): y  f(1)"  "" "The identifier x, redefined at ";
-  terr "multi_errs_duplicated_r" "def f(x, x): y  f(1)"  "" ", duplicates one at ";
-  terr "multi_errs_arity_mismatch" "def f(x, x): y  f(1)" ""  "arity mismatch in call";
-
-
+  terr "multi_errs_unbound_l" multi_errs_code_str "" "The identifier y, used at ";
+  terr "multi_errs_unbound_r" multi_errs_code_str "" ", is not in scope";
+  terr "multi_errs_duplicated_l" multi_errs_code_str "" "The identifier x, redefined at ";
+  terr "multi_errs_duplicated_r" multi_errs_code_str "" ", duplicates one at ";
 ]
 
 let diamondback_suite = "diamondback_suite">:::tests_from_diamondback
